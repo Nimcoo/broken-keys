0ee0a55cc2dee310a704eb1cc7124bbf657d71d5
NIMCOO <nimco77001@gmail.com>
2023-12-08 17:15:02 +0000

    Done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Good Luck 💪🏾
+ const BrokenNumer = function (phrase){
+ if("fikrcamp bootcamp" == "fikrcomp boorcamp"){
+
+ }else{
+    console.log(wrong)
+ }
+ 
+ }
+
+ return 'the correct sentence';
+  
+  BrokenNumer("fikrcamp bootcamp", "fikrcomp boorcamp")
+  console.log(correct)
